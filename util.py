fef639cc17ab69f737893c22ca6aa43ccd0486b1
ytongxue <ytongxue@gmail.com>
2018-08-22 22:01:11 +0800

    Modify util printToShell, use sys.__stdout__ for output

--- util.py
+++ util.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
 from datetime import datetime
-stdout = sys.stdout #backup stdout, avoid redirection
 def printToShell(*args):
     """
     print to shell
     """
     nowStr = datetime.now().strftime("%F %T.%f")[:-3]
-    stdout.write("[{}] ".format(nowStr))
+    sys.__stdout__.write("[{}] ".format(nowStr))
     line = " ".join(map(str, args))
-    stdout.write(line)
-    stdout.write("\n")
+    sys.__stdout__.write(line)
+    sys.__stdout__.write("\n")
